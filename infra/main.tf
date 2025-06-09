febca4f7241f8946fa36869560963f59a3cec047
Mohamamd Mustafa <mustafa.m3946@gmail.com>
2025-06-09 19:56:01 +1000

    Add Terraform configuration for Azure resources: define resource group and storage account

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "qa" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "qa_reports" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.qa.name
+  location                 = azurerm_resource_group.qa.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+}
+
+output "static_website_url" {
+  value = azurerm_storage_account.qa_reports.primary_web_endpoint
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "qa-playwright-rg"
+}
+
+variable "location" {
+  default = "australiaeast"
+}
+
+variable "storage_account_name" {
+  default = "qaplaywrightstorage"
+}
